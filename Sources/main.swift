2bb8b98047a40fd4c1f00472a3d0b181d5c1fe1b
maxie <maxie98@proton.me>
2025-04-08 04:32:28 -0500

    move entry point struct -> main file

---
+++ Sources/main.swift
@@ -0,0 +1,21 @@
+import DiscordBM
+
+let bot = await BotGatewayManager(
+    token: TODO("Token not implemented"),
+    presence: .init(
+        activities: [.init(name: "your tickets", type: .watching)], status: .online,
+        afk: false),
+    intents: [.messageContent, .guildMembers]
+)
+
+await withTaskGroup { group in
+    group.addTask {
+        await bot.connect()
+    }
+
+    group.addTask {
+        for await event in await bot.events {
+            await EventHandler(event: event, client: bot.client).handleAsync()
+        }
+    }
+}
